53e6407b1a394a9aa77f0fd01fd45b59cd152280
merlynjwa <merlyn.wa@gmail.com>
2022-07-06 17:32:36 +0000

    Add the hands dictionary
    Add a dictionary of dictionaries for each different hand which is
    possible in the game.
    Each sub-dictionary also contains an array of the hands which
    it defeats.
    
    I aim to use this to create a comparison function which will take two
    hands and return which one wins.

---
+++ assets/javascript/index.js
@@ -0,0 +1,7 @@
+const hands = {
+	rock : { hand : "rock", beats : ["scissors", "lizard"] },
+	paper : { hand : "paper", beats : ["rock", "spock"] },
+	scissors : { hand : "scissors", beats : ["paper", "lizard"] },
+	spock : { hand : "spock", beats : ["rock", "scissors"] },
+	lizard : { hand : "lizard", beats : ["paper", "spock"] }
+}
